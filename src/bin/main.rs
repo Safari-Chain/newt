3f0ef63d21a117a08b0d8dbe13dd684258720618
Vladimir Fomene <vladimirfomene@gmail.com>
2022-05-06 17:21:42 +0000

    move library code to lib.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -73,68 +73,8 @@ fn main() -> Result<()> {
         hexstring
     );
 
-    let tx = decode_txn(hexstring);
-    let analysis_result = check_multi_script(tx);
-    println!("Analysis result: {:?}", analysis_result);
-
     Ok(())
 }
-
-#[derive(Debug)]
-struct AnalysisResult {
-    heuristic: String,
-    result: bool,
-    scripts: Vec<String>,
-    details: String,
-}
-
-/// Multi-script heuristic
-// 1.) create function to collect and decode transaction hex(es) and
-// convert it to a transaction struct
-fn decode_txn(hex_str: String) -> Transaction {
-    let tx_bytes = hexfunc::decode(hex_str).unwrap();
-
-    let tx = bitcoin::blockdata::transaction::Transaction::deserialize(&tx_bytes).unwrap();
-    println!("transaction details: {:#?}", &tx);
-    return tx;
-}
-
-// 2. create function that converts scripts to addresses
-fn script_to_addr(script: Script) -> address::Address {
-    let addr = address::Address::from_script(&script, NETWORK).unwrap();
-    addr
-}
-
-fn get_address_type(vouts: Vec<TxOut>) -> Vec<AddressType> {
-    let address_type = vouts.into_iter().map(|vout| {
-        let addr = script_to_addr(vout.script_pubkey.clone());
-        let addr_type = address::Address::address_type(&addr).unwrap();
-        return addr_type;
-    }).collect();
-    return address_type;
-}
-
-// 3. check for multi-script types using addresses
-fn check_multi_script(txn: Transaction) -> AnalysisResult {
-    let outputs = txn.output;
-    let addr_types = get_address_type(outputs.clone()).clone();
-    let first_addr_type = addr_types.get(0).unwrap();
-    let result = outputs.into_iter().all(|vout| {
-        let addr = script_to_addr(vout.script_pubkey.clone());
-        let addr_type = address::Address::address_type(&addr).unwrap();
-        return addr_type == first_addr_type.clone();
-    });
-
-   let script_types: Vec<String> = addr_types.into_iter().map(|addr| addr.to_string()).collect();
-   let details = if result { "Multi-script" } else { "Single-script" };
-    return AnalysisResult {
-        heuristic: String::from("Mixed script heuristics!"),
-        result,
-        scripts: script_types,
-        details: String::from(details),
-    };
-}
-
 
 
 // We cache the pubkeys for convenience because it requires a scep context to convert the private key.
